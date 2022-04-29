69d0da1c2b684d1c810fc4cf35596fac061045e4
Manu <manuthecoder@protonmail.com>
2022-04-29 00:19:38 -0700

    Add redirect for index page

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+import { useEffect } from "react";
+import router from "next/router";
+export default function Render() {
+  useEffect(() => {
+    router.push("/oauth/eccbc87e4b5ce2fe28308fd9f2a7baf3");
+  });
+  return <></>;
+}
