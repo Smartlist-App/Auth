471ca479e1d4f8c0cf5ff48863bfec50b140b153
Manu <manuthecoder@protonmail.com>
2022-05-13 12:29:38 -0700

    Fix auth

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-import { useEffect } from "react";
-import router from "next/router";
-export default function Render() {
-  useEffect(() => {
-    router.push("/oauth/eccbc87e4b5ce2fe28308fd9f2a7baf3");
-  });
-  return <></>;
-}
