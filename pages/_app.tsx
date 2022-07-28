c614d1e4d64348bf4f41799b7242e5658feeb6c4
Manu <manuthecoder@protonmail.com>
2022-07-28 20:20:39 +0000

    update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -23,6 +23,18 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <title>Login &bull; Carbon</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Kantumruy+Pro:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://i.ibb.co/2snZjPZ/48x48-modified-1.png"
+          rel="shortcut icon"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.sandbox.google.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@40,500,1,200"
+        />
       </Head>
       <ThemeProvider theme={darkTheme}>
         <Toaster />
